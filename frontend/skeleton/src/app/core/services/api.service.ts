5b9727d1c348f4c1476a6b6bbb8df68f6b9b9f64
Julián Salgado <srjulianc25@gmail.com>
2025-02-02 21:26:54 -0500

    Implements some crud methods

--- frontend/skeleton/src/app/core/services/api.service.ts
+++ frontend/skeleton/src/app/core/services/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = 'http://localhost:8000/api'; // URL de Laravel
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get(`${this.apiUrl}/products`);
+  }
+
+  createProduct(product: any) {
+    return this.http.post(`${this.apiUrl}/products`, product);
+  }
+
+  // TODO: faltan metodos para actualizar y borrar
 }
