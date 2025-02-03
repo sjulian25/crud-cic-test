ac71099290ec5809813914b5b09ffd065e525b2e
Julián Salgado <srjulianc25@gmail.com>
2025-02-02 21:36:49 -0500

    config routes

--- frontend/skeleton/src/app/app.routes.ts
+++ frontend/skeleton/src/app/app.routes.ts
@@ -1,10 +1,29 @@
 import { LayoutComponent } from './layouts/layout/layout.component';
 import { VexRoutes } from '@vex/interfaces/vex-route.interface';
+import { Routes } from '@angular/router';
+import { ProductListComponent } from './pages/products/product-list/product-list.component';
+import { ProductFormComponent } from './pages/products/product-form/product-form.component';
 
 export const appRoutes: VexRoutes = [
   {
     path: '',
     component: LayoutComponent,
     children: []
+  }
+];
+
+export const routes: Routes = [
+  // ... Otras rutas existentes
+  { 
+    path: 'products',
+    component: ProductListComponent 
+  },
+  { 
+    path: 'products/create',
+    component: ProductFormComponent 
+  },
+  { 
+    path: 'products/edit/:id',
+    component: ProductFormComponent 
   }
 ];
