a4af0cd4f660fd916cd54322bc4ab1326dba6491
Julián Salgado <srjulianc25@gmail.com>
2025-02-02 22:18:01 -0500

    fix imports in product-list component

--- frontend/skeleton/src/app/pages/products/product-list/product-list.component.ts
+++ frontend/skeleton/src/app/pages/products/product-list/product-list.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { VexPageLayoutComponent } from '@vex/components/vex-page-layout/vex-page-layout.component';
+import { VexPageLayoutContentDirective } from '@vex/components/vex-page-layout/vex-page-layout-content.directive';
+import { VexPageLayoutHeaderDirective } from '@vex/components/vex-page-layout/vex-page-layout-header.directive';
 
 @Component({
   selector: 'vex-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    VexPageLayoutComponent,
+    VexPageLayoutContentDirective,
+    VexPageLayoutHeaderDirective
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
