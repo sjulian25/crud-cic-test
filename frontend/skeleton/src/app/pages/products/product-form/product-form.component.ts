b61c62b48a779387f96488c545048b4755816a60
Julián Salgado <srjulianc25@gmail.com>
2025-02-02 21:35:12 -0500

    create crud components

---
+++ frontend/skeleton/src/app/pages/products/product-form/product-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vex-product-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.scss'
+})
+export class ProductFormComponent {
+
+}
---
+++ frontend/skeleton/src/app/pages/products/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vex-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+
+}
